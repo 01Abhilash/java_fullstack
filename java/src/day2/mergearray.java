8d25a65f052a1d9ef88164ff8ec38274f0f28fa4
Abhilash <kra84195@gmail.com>
2025-07-30 10:33:27 +0530

    updated

--- java/src/day2/mergearray.java
+++ java/src/day2/mergearray.java
@@ -1,10 +1,21 @@
 package day2;
-import java.util.Scanner;
+import java.util.Arrays;
 public class mergearray {
-	public static void main(String[]args) {
-		int[] array1 = {1, 2, 3, 4, 5};
-		int[] array2= {6,7,8,9,10};
-		length1=arr.
-	}
-
-}
+	public static void main(String[] args) {
+		int[] arr1= {5,10,15,20,25};
+		int [] arr2= {30,35,40,45,50};
+		int l1=arr1.length;
+		int l2=arr2.length;
+		int arr3[]=new int[l1+l2] ;
+		for(int i=0;i<l1;i++) 
+			arr3[i]=arr1[i];
+		for(int i=0;i<l2;i++) 
+			arr3[l1+i]=arr2[i];
+		
+		
+		System.out.println("Merged Array: " + Arrays.toString(arr3));
+		Arrays.sort(arr3);
+		System.out.println("Ascending order: "+Arrays.toString(arr3));
+		
+		}
+		}
